0e3d52b67a033c1e70ad8fcea6a81d41a3f1acd5
maximdushniak <maxim.dushniak@gmail.com>
2016-09-30 18:56:44 +0300

    вывод кириллицы

--- pprint_json.py
+++ pprint_json.py
@@ -10,7 +10,7 @@ def load_data(filepath):
 
 def pretty_print_json(data):
     # json.dumps(data, sort_keys=True, indent=4)
-    print(json.dumps(data, sort_keys=True, indent=4))
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
