cf5905cb06e07f565fb7656e9bb80b7a2c2049e5
maximdushniak <maxim.dushniak@gmail.com>
2016-09-30 16:40:05 +0300

    выбор файла данных

--- pprint_json.py
+++ pprint_json.py
@@ -2,7 +2,10 @@ import json
 
 
 def load_data(filepath):
-    pass
+    data = []
+    with open(filepath) as datafile:
+        data = json.loads(datafile.read())
+    return data
 
 
 def pretty_print_json(data):
@@ -10,4 +13,8 @@ def pretty_print_json(data):
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input(u'Имя файла [data.json]: ')
+    if not filepath:
+        filepath = 'data.json'
+    data = load_data(filepath)
+    pretty_print_json(data)
