ac3c70319306f02ae1e3f260a00c930fad3b7a14
maximdushniak <maxim.dushniak@gmail.com>
2016-10-03 19:59:03 +0300

    readme

--- pprint_json.py
+++ pprint_json.py
@@ -2,14 +2,17 @@ import json
 
 
 def load_data(filepath):
-    data = []
-    with open(filepath, encoding='utf-8') as datafile:
-        data = json.load(datafile)
-    return data
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r', encoding='utf-8') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
+    if data is None:
+        print(data)
+    else:
+        print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
